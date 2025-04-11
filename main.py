19ad8a51c40283545935ba51160b5e7e006de152
cetaylor07 <cetaylor07@gmail.com>
2025-04-10 23:31:40 -0400

    Initial commit - implemented game loop with black screen

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import *
+
+def __main__():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    __main__()
